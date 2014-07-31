4fe285b3cb05c5de2b482e8f4219742f45e3b6a9
Andreas R?ver <roever@users.sourceforge.net>
2014-07-31 21:29:53 +0200

    update main file
    Ignore-this: 9c9d1dea0c371d8170b07add82f04cc4
    
    darcs-hash:20140731192953-c09e1-fb9774f97fde215c4f5821e1a24da6bd02b3d6b3

--- main.cpp
+++ main.cpp
@@ -1,136 +1,71 @@
 #include "layouter.h"
 #include "layouterSDL.h"
 
-#define NUM_EXAMPLES 3
-
-#include <hb.h>
-#include <hb-ft.h>
-
-/* tranlations courtesy of google */
-const char *texts[NUM_EXAMPLES] = {
-    "Test ЛенивыйVA",
-    "Test كسول 123 الزنجبيل القط التعليقات",
-//    "Test באתר 123 בנייה, העגורן הוא"
-//    "Test الزنجبي",
-//    "懶惰的姜貓",
-};
-
-// a trivial example
+#define TXT_WIDTH 700
+#define WIN_WIDTH 800
 
 int main ()
 {
   textStyleSheet_c styleSheet;
 
-  styleSheet.language="ar-Arab";
-  styleSheet.font("sans", "fonts/amiri-0.104/amiri-regular.ttf");
-//  styleSheet.font("sans", "/usr/share/fonts/freefont/FreeSans.ttf");
+  // setze default sprache des textes
+  styleSheet.language="en-Engl";
 
+  // alle Fonts, die so genutzt werden: familie heißt sans, und dann der bold Font dazu
+  styleSheet.font("sans", "/usr/share/fonts/freefont/FreeSans.ttf");
+  styleSheet.font("sans", "/usr/share/fonts/freefont/FreeSansBold.ttf", "normal", "normal", "bold");
+
+  // CSS regeln, immer Selector, attribut, wert
+  styleSheet.addRule("body", "color", "#ffffff");
+  styleSheet.addRule("body", "font-family", "sans");
+  styleSheet.addRule("body", "font-style", "normal");
+  styleSheet.addRule("body", "font-size", "30px");
+  styleSheet.addRule("body", "font-variant", "normal");
+  styleSheet.addRule("body", "font-weight", "normal");
+  styleSheet.addRule("body", "text-align", "justify");
+  styleSheet.addRule(".BigFont", "font-size", "35px");
+  styleSheet.addRule(".BoldFont", "font-weight", "bold");
+  styleSheet.addRule("i", "color", "#ff0000");
+  styleSheet.addRule("h1", "font-weight", "bold");
+  styleSheet.addRule("h1", "font-size", "60px");
+  styleSheet.addRule("h1", "text-align", "center");
+
+  // der zu formatierende text
+  std::string text = u8"<html><body>"
+    "<h1>Überschrift</h1><p>Test <i>Text</i> more and some"
+    "<div class=\"BoldFont\">more text so</div> that the pa\u00ADra\u00ADgraph is at least "
+    "<div>long</div> enough to span some lines on the screen let us "
+    "also <i>i</i>nclude sm hebrew נייה, העגורן הוא and back to english</p>"
+    "<p>2nd Paragraph<div class=\"BigFont\"> with <div class=\"BoldFont\">a\ndivision</div>"
+    "</div>.</p>"
+    "<p>a b c andnowone<i>very</i>longwordthatdoesntfitononelineandmight d e f °C</p>"
+    "<ul><li>First long text in an ul list of html sdfsd fsd fs dfsd f gobble di gock and"
+    "even more</li><li>Second with just a bit</li></ul>"
+    "</body></html>";
+
+  // Vektor mit auszugebenden Text layouts, besteht immer aus einem layoute
+  // und dessen Position auf dem Bildschirm
   std::vector<layoutInfo_c> l;
 
-  l.emplace_back(layoutInfo_c(layout(texts[1], styleSheet, rectangleShape_c(250)), 10, 80));
+  // grauer Hintergrund, damit ich sehen kann, ob der Text die korrekte Breite hat
+  // erzeuge ein künstliches Layout nur mit einem Rechteck drin... so etwas wird später
+  // nicht mehr benötigt, ist nur für den Test
+  textLayout_c la;
+  textLayout_c::commandData c;
+  c.command = textLayout_c::commandData::CMD_RECT;
+  c.x = c.y = 0;
+  c.w = TXT_WIDTH;
+  c.h = 600;
+  c.r = c.g = c.b = 50;
+  la.addCommand(c);
+  l.emplace_back(layoutInfo_c(la, (WIN_WIDTH-TXT_WIDTH)/2, 20));
 
-  showLayoutsSelf(800, 600, l);
+  // das eigentliche Layout
+  // layoutXHTML macht die Arbeit, übergeben wird der Text, das Stylesheet und eine Klasse,
+  // die die Form des Textes beinhaltet (für nicht rechteckiges Layout
+  l.emplace_back(layoutInfo_c(layoutXHTML(text, styleSheet, rectangleShape_c(TXT_WIDTH)),
+                              (WIN_WIDTH-TXT_WIDTH)/2, 20));
+
+  // Ausgabe mittels SDL
+  showLayoutsSelf(WIN_WIDTH, 600, l);
 }
-
-
-#if 0
-//a slightly more complex example
-int main ()
-{
-  auto fontcache = std::make_shared<fontCache_c>();
-  std::vector<textStyleSheet_c> styleSheets;
-  std::vector<fontFamilyCollection_c> fontFamilies;
-
-  textStyleSheet_c ss;
-  fontFamilyCollection_c c;
-
-  styleSheets.push_back(ss);
-  c["sans"] = std::make_shared<fontFamily_c>(fontcache);
-  c["sans"]->addFont("fonts/DejaVuSerif.ttf");
-  fontFamilies.push_back(c);
-
-  ss.language="ar-Arab";
-  styleSheets.push_back(ss);
-  c["sans"] = std::make_shared<fontFamily_c>(fontcache);
-  c["sans"]->addFont("fonts/amiri-0.104/amiri-regular.ttf");
-  fontFamilies.push_back(c);
-
-  ss.language="ch-Hani";
-  styleSheets.push_back(ss);
-  c["sans"] = std::make_shared<fontFamily_c>(fontcache);
-  c["sans"]->addFont("fonts/fireflysung-1.3.0/fireflysung.ttf");
-  fontFamilies.push_back(c);
-
-  std::vector<textLayout_c> layouts;
-
-  layouts.emplace_back(layout(texts[0], styleSheets[0], fontFamilies[0]));
-  layouts.emplace_back(layout(texts[1], styleSheets[1], fontFamilies[1]));
-  layouts.emplace_back(layout(texts[2], styleSheets[2], fontFamilies[2]));
-
-  /** Setup our SDL window **/
-  int width      = 800;
-  int height     = 600;
-  int videoFlags = SDL_SWSURFACE | SDL_DOUBLEBUF;
-  int bpp        = 32;
-
-  /* Initialize our SDL window */
-  if(SDL_Init(SDL_INIT_VIDEO) < 0)   {
-    fprintf(stderr, "Failed to initialize SDL");
-    return -1;
-  }
-
-  SDL_WM_SetCaption("\"Simple\" SDL+FreeType+HarfBuzz Example", "\"Simple\" SDL+FreeType+HarfBuzz Example");
-
-  SDL_Surface *screen;
-  screen = SDL_SetVideoMode(width, height, bpp, videoFlags);
-
-  /* Enable key repeat, just makes it so we don't have to worry about fancy
-   * scanboard keyboard input and such */
-  SDL_EnableKeyRepeat(300, 130);
-  SDL_EnableUNICODE(1);
-
-  /* Create an SDL image surface we can draw to */
-  SDL_Surface *sdl_surface = SDL_CreateRGBSurface (0, width, height, 32, 0,0,0,0);
-
-  /* Clear our surface */
-  SDL_FillRect( sdl_surface, NULL, 0 );
-  SDL_LockSurface(sdl_surface);
-
-  SDL_UnlockSurface(sdl_surface);
-
-  showLayoutSDL(layouts[0], 10, 80 + 0 * 105, sdl_surface);
-  showLayoutSDL(layouts[1], 10, 80 + 1 * 105, sdl_surface);
-  showLayoutSDL(layouts[2], 10, 80 + 2 * 105, sdl_surface);
-
-  /* Blit our new image to our visible screen */
-
-  SDL_BlitSurface(sdl_surface, NULL, screen, NULL);
-  SDL_Flip(screen);
-
-  /* Our main event/draw loop */
-  int done = 0;
-
-  while (!done)
-  {
-
-    /* Handle SDL events */
-    SDL_Event event;
-    while(SDL_PollEvent(&event))
-    {
-      switch (event.type)
-      {
-        case SDL_KEYDOWN:
-        case SDL_QUIT:
-          done = 1;
-          break;
-      }
-    }
-
-    SDL_Delay(150);
-  }
-
-  SDL_FreeSurface(sdl_surface);
-
-  SDL_Quit();
-}
-#endif
