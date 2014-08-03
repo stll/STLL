67c38a663880ed2bf0faa57f187e16bea4dece50
Andreas R?ver <roever@users.sourceforge.net>
2014-08-03 10:11:19 +0200

    some cleanup in the SDL output, also implement proper alpha
    Ignore-this: 2ebac3067f91289f144b13baefc931b
    
    darcs-hash:20140803081119-c09e1-56eebe1b9c1be07eb5f805611959c5d053eceb34

--- layouterSDL.cpp
+++ layouterSDL.cpp
@@ -1,48 +1,56 @@
 #include "layouterSDL.h"
 
-typedef struct _spanner_baton_t {
-    /* rendering part - assumes 32bpp surface */
-    uint32_t *pixels; // set to the glyph's origin.
-    uint32_t *first_pixel, *last_pixel; // bounds check
-    uint32_t pitch;
-    uint32_t rshift;
-    uint32_t gshift;
-    uint32_t bshift;
-    uint32_t ashift;
+typedef struct
+{
+  // set to the origin of the pixel
+  uint32_t *pixels;
 
-    uint8_t r, g, b;
+  // final bounding check
+  uint32_t *first_pixel, *last_pixel;
 
-} spanner_baton_t;
+  // format information
+  int32_t pitch;
+  uint32_t rshift;
+  uint32_t gshift;
+  uint32_t bshift;
+  uint32_t ashift;
 
-/* This spanner does read/modify/write, trading performance for accuracy.
-   The color here is simply half coverage value in all channels,
-   effectively mid-gray.
-   Suitable for when artifacts mostly do come up and annoy.
-   This might be optimized if one does rmw only for some values of x.
-   But since the whole buffer has to be rw anyway, and the previous value
-   is probably still in the cache, there's little point to. */
+  uint8_t r, g, b, a;
+
+} spanInfo;
+
 void spanner(int y, int count, const FT_Span* spans, void *user)
 {
-  spanner_baton_t *baton = (spanner_baton_t *) user;
+  spanInfo *baton = (spanInfo *)user;
 
-  uint32_t *scanline = baton->pixels - y * ( (int) baton->pitch / 4 );
+  uint32_t *scanline = baton->pixels - y * baton->pitch;
 
   if (scanline >= baton->first_pixel)
   {
     for (int i = 0; i < count; i++)
     {
-      uint32_t color =
-      ((spans[i].coverage*baton->r/256) << baton->rshift) |
-      ((spans[i].coverage*baton->g/256) << baton->gshift) |
-      ((spans[i].coverage*baton->b/256) << baton->bshift);
-
       uint32_t *start = scanline + spans[i].x;
 
       if (start + spans[i].len >= baton->last_pixel)
         return;
 
+      uint8_t alpha = (spans[i].coverage * baton->a) / 255;
+
       for (int x = 0; x < spans[i].len; x++)
-        *start++ |= color;
+      {
+        uint8_t pr = *start >> baton->rshift;
+        uint8_t pg = *start >> baton->gshift;
+        uint8_t pb = *start >> baton->bshift;
+
+        pr = ((255-alpha)*pr + (alpha*baton->r))/255;
+        pg = ((255-alpha)*pg + (alpha*baton->g))/255;
+        pb = ((255-alpha)*pb + (alpha*baton->b))/255;
+
+        *start = (pr << baton->rshift) | (pg << baton->gshift) | (pb << baton->bshift);
+        start++;
+
+        if (start >= baton->last_pixel) return;
+      }
     }
   }
 }
@@ -50,20 +58,19 @@ void spanner(int y, int count, const FT_Span* spans, void *user)
 void showLayoutSDL(const textLayout_c & l, int sx, int sy, SDL_Surface * s)
 {
   /* set up rendering via spanners */
-  spanner_baton_t stuffbaton;
-  stuffbaton.pixels = NULL;
-  stuffbaton.first_pixel = (uint32_t*)s->pixels;
-  stuffbaton.last_pixel = (uint32_t *) (((uint8_t *) s->pixels) + s->pitch*s->h);
-  stuffbaton.pitch = s->pitch;
-  stuffbaton.rshift = s->format->Rshift;
-  stuffbaton.gshift = s->format->Gshift;
-  stuffbaton.bshift = s->format->Bshift;
-
+  spanInfo span;
+  span.pixels = NULL;
+  span.first_pixel = (uint32_t*)s->pixels;
+  span.last_pixel = (uint32_t *) (((uint8_t *) s->pixels) + s->pitch*s->h);
+  span.pitch = s->pitch/4;
+  span.rshift = s->format->Rshift;
+  span.gshift = s->format->Gshift;
+  span.bshift = s->format->Bshift;
 
   FT_Raster_Params ftr_params;
   ftr_params.target = 0;
   ftr_params.flags = FT_RASTER_FLAG_DIRECT | FT_RASTER_FLAG_AA;
-  ftr_params.user = &stuffbaton;
+  ftr_params.user = &span;
   ftr_params.black_spans = 0;
   ftr_params.bit_set = 0;
   ftr_params.bit_test = 0;
@@ -77,10 +84,11 @@ void showLayoutSDL(const textLayout_c & l, int sx, int sy, SDL_Surface * s)
     {
       case textLayout_c::commandData::CMD_GLYPH:
 
-        stuffbaton.pixels = (uint32_t *)(((uint8_t *) s->pixels) + (sy+i.y) * s->pitch) + (sx+i.x);
-        stuffbaton.r = i.r;
-        stuffbaton.g = i.g;
-        stuffbaton.b = i.b;
+        span.pixels = (uint32_t *)(((uint8_t *) s->pixels) + (sy+i.y) * s->pitch) + (sx+i.x);
+        span.r = i.r;
+        span.g = i.g;
+        span.b = i.b;
+        span.a = i.a;
 
         i.font->outlineRender(i.glyphIndex, &ftr_params);
 
