a70fb18c0acd12a052c20bcc78c29fb1459dd33d
Andreas Röver <roever@users.sf.net>
2014-09-20 16:01:56 +0200

    update doxygen documentation

--- class_s_t_l_l_1_1font_face__c.js
+++ class_s_t_l_l_1_1font_face__c.js
@@ -4,9 +4,9 @@ var class_s_t_l_l_1_1font_face__c =
     [ "~fontFace_c", "class_s_t_l_l_1_1font_face__c.html#a63315737f2c8dfdc727c5a1a6913ce8a", null ],
     [ "getAscender", "class_s_t_l_l_1_1font_face__c.html#a8dba9fb92d02f6925f4c27ecc7002ec0", null ],
     [ "getDescender", "class_s_t_l_l_1_1font_face__c.html#a98d78349848d45985393884d3140bd26", null ],
-    [ "getFace", "class_s_t_l_l_1_1font_face__c.html#af29ca316868f4c1862c232ff0cb271f3", null ],
+    [ "getFace", "class_s_t_l_l_1_1font_face__c.html#a04a41f826ab38fde989a1e82cf354ac1", null ],
     [ "getHeight", "class_s_t_l_l_1_1font_face__c.html#ab400183f583ce7f840a942d24271d05a", null ],
     [ "getUnderlinePosition", "class_s_t_l_l_1_1font_face__c.html#a45617e8d2cdfba175304692e25c1b5f0", null ],
     [ "getUnderlineThickness", "class_s_t_l_l_1_1font_face__c.html#a19dfa387fa2045fdd4d0dafe639c7e26", null ],
-    [ "outlineRender", "class_s_t_l_l_1_1font_face__c.html#aa7e720d4756d3ae40758b9e3c28d8c51", null ]
+    [ "outlineRender", "class_s_t_l_l_1_1font_face__c.html#aa1d35f98b19267a8b6a0751e32dc8d47", null ]
 ];
